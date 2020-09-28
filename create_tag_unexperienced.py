f129d7e9e7b5f09676ee7227ad09a24d558b73df
TakeKiyo <d8sk4ueum@gmail.com>
2020-09-28 16:38:08 +0900

    技術領域と言語を、配列じゃなくて文字列で持つようにした

--- create_tag_unexperienced.py
+++ create_tag_unexperienced.py
@@ -37,7 +37,21 @@ for row in df.itertuples():
         t = driver.find_element_by_xpath('//*[@id="project-show-body"]/div/div[1]/div/div').text
         m = MeCab.Tagger()
         nouns = [line.split("\t")[0] for line in m.parse(t).splitlines()]
-        OutputList.append([row[2],row[3],row[4],row[5],list(set(nouns) & set(domainList)),row[7],list(set(nouns) & set(TechnologyList)),t])
+        commonDomain = list(set(nouns) & set(domainList))
+        domain=""
+        if len(commonDomain)!=0:
+            for i in range(len(commonDomain)):
+                if i != 0:
+                    domain += ","
+                domain += str(commonDomain[i])
+        commonTech = list(set(nouns) & set(TechnologyList))
+        tech = ""
+        if len(commonTech) != 0:
+            for i in range(len(commonTech)):
+                if i != 0:
+                    tech += ","
+                tech += str(commonTech[i])
+        OutputList.append([row[2],row[3],row[4],row[5],domain,row[7],tech,t])
     else:   
         driver.get(row[3])
         if len(driver.find_elements_by_xpath('//*[@id="select-occupation-type"]/div[1]/span/i')) > 0:
@@ -46,7 +60,21 @@ for row in df.itertuples():
         t = driver.find_element_by_xpath('//*[@id="project-show-body"]/div/div[1]/div/div').text
         m = MeCab.Tagger()
         nouns = [line.split("\t")[0] for line in m.parse(t).splitlines()]
-        OutputList.append([row[2],row[3],row[4],row[5],list(set(nouns) & set(domainList)),row[7],list(set(nouns) & set(TechnologyList)),t])
+        commonDomain = list(set(nouns) & set(domainList))
+        domain=""
+        if len(commonDomain)!=0:
+            for i in range(len(commonDomain)):
+                if i != 0:
+                    domain += ","
+                domain += str(commonDomain[i])
+        commonTech = list(set(nouns) & set(TechnologyList))
+        tech = ""
+        if len(commonTech) != 0:
+            for i in range(len(commonTech)):
+                if i != 0:
+                    tech += ","
+                tech += str(commonTech[i])
+        OutputList.append([row[2],row[3],row[4],row[5],domain,row[7],tech,t])
 df = pd.DataFrame(OutputList,columns=["会社名","URL","タイトル","経験","技術領域","形式","言語タグ","本文"])
 df.to_csv("output/unexperienced.csv",encoding='utf_8_sig')
 
