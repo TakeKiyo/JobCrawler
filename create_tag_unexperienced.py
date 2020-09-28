b8f8fd962293992c47244b36c14ff81966f583d8
TakeKiyo <d8sk4ueum@gmail.com>
2020-09-28 15:37:18 +0900

    クローリングしたcsvから、 DBにインポートする情報を整理

---
+++ create_tag_unexperienced.py
@@ -0,0 +1,56 @@
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import MeCab
+import pandas as pd
+
+TechnologyList = ["C","C++","C#","Java","Go","Ruby","Python","Javascript","Typescript","React","Vue","jQuery","Rails","Django","fortran","Swift","Kotlin","React Native"]
+domainList = ["バックエンド","フロントエンド","インフラ","iOS","android"]
+OutputList = []
+
+df = pd.read_csv("unexperienced.csv")
+idx = 0
+driver = webdriver.Chrome("/usr/local/bin/chromedriver")
+for row in df.itertuples():
+    if idx==0:
+        idx += 1
+        driver.get(row[3])
+        time.sleep(1)
+        driver.find_element_by_id("project-description-signin").click()
+        time.sleep(1)
+        driver.find_element_by_css_selector(".email-signin.login-button.new-ui-button-inverted-cleared").click()
+        time.sleep(1)
+        mail_input = driver.find_element_by_xpath('//*[@id="email"]')
+        time.sleep(1)
+        mail_input.send_keys("oldmaid138@gmail.com")
+        driver.find_element_by_id("next-step-button").click()
+        time.sleep(1)
+        pass_input = driver.find_element_by_xpath('//*[@id="password"]')
+        pass_input.send_keys("password")
+        driver.find_element_by_xpath('//*[@id="next-step-button"]').click()
+        time.sleep(3)
+        if len(driver.find_elements_by_xpath('//*[@id="select-occupation-type"]/div[1]/span/i')) > 0:
+            driver.find_element_by_xpath('//*[@id="select-occupation-type"]/div[1]/span/i').click()
+        time.sleep(1)
+        t = driver.find_element_by_xpath('//*[@id="project-show-body"]/div/div[1]/div/div').text
+        m = MeCab.Tagger()
+        nouns = [line.split("\t")[0] for line in m.parse(t).splitlines()]
+        OutputList.append([row[2],row[3],row[4],row[5],list(set(nouns) & set(domainList)),row[7],list(set(nouns) & set(TechnologyList)),t])
+    else:   
+        driver.get(row[3])
+        if len(driver.find_elements_by_xpath('//*[@id="select-occupation-type"]/div[1]/span/i')) > 0:
+            driver.find_element_by_xpath('//*[@id="select-occupation-type"]/div[1]/span/i').click()
+        time.sleep(3)
+        t = driver.find_element_by_xpath('//*[@id="project-show-body"]/div/div[1]/div/div').text
+        m = MeCab.Tagger()
+        nouns = [line.split("\t")[0] for line in m.parse(t).splitlines()]
+        OutputList.append([row[2],row[3],row[4],row[5],list(set(nouns) & set(domainList)),row[7],list(set(nouns) & set(TechnologyList)),t])
+df = pd.DataFrame(OutputList,columns=["会社名","URL","タイトル","経験","技術領域","形式","言語タグ","本文"])
+df.to_csv("output/unexperienced.csv",encoding='utf_8_sig')
+
+
+driver.quit()
+
+
