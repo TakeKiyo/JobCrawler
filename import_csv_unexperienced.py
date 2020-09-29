b27bb452c8298460566bed2ebba87e9a8cd2a668
TakeKiyo <d8sk4ueum@gmail.com>
2020-09-29 15:00:27 +0900

    DBにダンプするスクリプト書いた

--- import_csv_unexperienced.py
+++ import_csv_unexperienced.py
@@ -1,6 +1,6 @@
 import mysql.connector
 import pandas as pd 
-import math
+import time 
 
 # コネクションの作成
 conn = mysql.connector.connect(
@@ -14,42 +14,44 @@ conn.ping(reconnect=True)
 print("接続: "+str(conn.is_connected()))
 
 df = pd.read_csv("output/unexperienced.csv")
-experience_word = ["未経験可","Web開発経験","スマホアプリ開発経験"]
+experience_word = ["未経験可","Web開発経験","スマホアプリ開発経験","競プロ経験"]
 technology_word  = ["C","C++","C#","Java","Go","Ruby","Python","Javascript","Typescript","React","Vue","jQuery","Rails","Django","fortran","Swift","Kotlin","React Native"]
 style_word = ["実務経験","体験"]
-domain_word = ["バックエンド","フロントエンド","インフラ","iOS","android"]
+domain_word = ["バックエンド","フロントエンド","インフラ","iOS","Android"]
+# indexでidを返すときは、+1しないといけない
 cur = conn.cursor(buffered=True)
 for row in df.itertuples():
     try:
-        #recruiting_infoに登録
-        cur.execute("INSERT INTO gs_recruiting_items (name, content) VALUES (%s, %s)", (row[4],row[9]))
+        #engineer_internsに登録
+        cur.execute("INSERT INTO engineer_interns (title, content, company, entry_button_url, created, modified) values (%s, %s, %s, %s,%s,%s)",(row[4],row[9],row[2],row[3],time.strftime('%Y-%m-%d %H:%M:%S'),time.strftime('%Y-%m-%d %H:%M:%S')))
         conn.commit()
         cur.execute("SELECT last_insert_id() FROM gs_recruiting_item_experiences")
-        gs_recruiting_item_id = cur.fetchone()[0]
-        #recruiting_itemsに登録
+        engineer_intern_id = cur.fetchone()[0]
 
-        #gs_recruiting_item_experiencesに登録
+        #engineer_intern_experiencesに登録
         if type(row[5]) != float: #nanの判定
-            cur.execute("INSERT INTO gs_recruiting_item_experiences (gs_recruiting_item_id, experience) VALUES (%s, %s)", (gs_recruiting_item_id, experience_word.index(row[5])))
+            cur.execute("INSERT INTO engineer_intern_experiences (engineer_intern_id, experience) VALUES (%s, %s)", (engineer_intern_id, experience_word.index(row[5])+1))
         
-
-        #gs_recruiting_item_technologies
+        #engineer_intern_technologies
         if type(row[8]) != float: #nanの判定
             technology_word_list = row[8].split(",")
             for item in technology_word_list:
-                cur.execute("INSERT INTO gs_recruiting_item_technologies (gs_recruiting_item_id, technology) VALUES (%s, %s)", (gs_recruiting_item_id, technology_word.index(item)))
+                cur.execute("INSERT INTO engineer_intern_technologies (engineer_intern_id, technology) VALUES (%s, %s)", (engineer_intern_id, technology_word.index(item)+1))
         
-        #gs_recruiting_item_stylesに登録
+        #engineer_intern_styles
         if type(row[7]) != float: #nanの判定
-            cur.execute("INSERT INTO gs_recruiting_item_styles (gs_recruiting_item_id, style) VALUES (%s, %s)", (gs_recruiting_item_id, style_word.index(row[7])))
+            cur.execute("INSERT INTO engineer_intern_styles (engineer_intern_id, style) VALUES (%s, %s)", (engineer_intern_id, style_word.index(row[7])+1))
         
-        #gs_recruiting_item_domainsに登録
+        #engineer_intern_domains
         if type(row[6]) != float: #nanの判定
             domain_word_list = row[6].split(",")
             for item in domain_word_list:
-                cur.execute("INSERT INTO gs_recruiting_item_domains (gs_recruiting_item_id, domain) VALUES (%s, %s)", (gs_recruiting_item_id, domain_word.index(item)))
+                print("aaaa")
+                cur.execute("INSERT INTO engineer_intern_domains (engineer_intern_id, domain) VALUES (%s, %s)", (engineer_intern_id, domain_word.index(item)+1))
 
         conn.commit()
+        break
     except:
         conn.rollback()
         raise
+    break
